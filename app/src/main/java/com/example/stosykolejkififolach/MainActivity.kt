02ec5e8172138d8973e37a51e551b396e6239715
kamcio420 <kamcio420@gmail.com>
2022-10-29 00:28:41 +0200

    stack part code logic

--- app/src/main/java/com/example/stosykolejkififolach/MainActivity.kt
+++ app/src/main/java/com/example/stosykolejkififolach/MainActivity.kt
@@ -1,51 +1,63 @@
 package com.example.stosykolejkififolach
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Stack part
 
-        val stackElementsOutput = findViewById<TextView>(R.id.stackValText).text
-        val stackOutput = findViewById<TextView>(R.id.outputTextStack).text
+        var stackElementsOutput = findViewById<TextView>(R.id.stackValText)
+        var stackOutput = findViewById<TextView>(R.id.outputTextStack)
+        var inputStack = findViewById<TextInputEditText>(R.id.inputStack)
 
-        val inputStack = findViewById<TextInputEditText>(R.id.inputStack).text
+        var stack = ArrayDeque<Int>()
 
         findViewById<Button>(R.id.addButtonStack).setOnClickListener {
-
+            if(inputStack.text?.isNotEmpty() == true) {
+                stack.push(inputStack.text.toString().toInt())
+                stackElementsOutput.text = stack.toString()
+            }
         }
 
         findViewById<Button>(R.id.popButtonStack).setOnClickListener {
-
+            if(!stack.isEmpty()) {
+                stack.pop()
+                stackElementsOutput.text = stack.toString()
+            }
         }
 
         findViewById<Button>(R.id.topButtonStack).setOnClickListener {
-
+            if(!stack.isEmpty()) {
+                stackOutput.text = stack.peek().toString()
+            }
         }
 
         findViewById<Button>(R.id.sizeButtonStack).setOnClickListener {
-
+            stackOutput.text = stack.size.toString()
         }
 
         findViewById<Button>(R.id.emptyButtonStack).setOnClickListener {
-
+            if(stack.isEmpty()) {
+                stackOutput.text = "Yes"
+            }
+            else if(!stack.isEmpty()) {
+                stackOutput.text = "No"
+            }
         }
 
         //Queue part
 
-        val queueElementsOutput = findViewById<TextView>(R.id.queueValText).text
-        val queueOutput = findViewById<TextView>(R.id.outputTextQueue).text
-
-        val queueStack = findViewById<TextInputEditText>(R.id.inputQueue).text
+        var queueElementsOutput = findViewById<TextView>(R.id.queueValText)
+        var queueOutput = findViewById<TextView>(R.id.outputTextQueue)
+        var queueStack = findViewById<TextInputEditText>(R.id.inputQueue)
 
         findViewById<Button>(R.id.addButtonQueue).setOnClickListener {
 
